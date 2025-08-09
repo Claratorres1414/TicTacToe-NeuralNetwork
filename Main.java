341a2ef43f8a854fe1fdf556be22dfa75c336059
claratorres1414 <claratorres12345679@gmail.com>
2025-08-09 08:31:15 -0300

    Primeiro commit do projeto em funcionamento, por enquanto a rede ainda não aprende, mas a rede já está implementada e rodando

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static final int human = -1;
+    private static final int computer = 1;
+    private static NeuralNetwork neuralNetwork = new NeuralNetwork(9, 10, 9);
+
+    public static void main(String[] args) {
+        //boot
+        while(true) {
+            TicTacToe game = new TicTacToe();
+            System.out.println("Let's start a new Tic-Tac-Toe game!");
+            //if the game does not have a winner or is not full
+            while(game.checkWinner() == 0 && !game.isFull()) {
+                game.humanTurn();
+
+                if(game.checkWinner() == human) {
+                    game.printBoard();
+                    System.out.println("Congratulations! You won!");
+                    break;
+                }
+
+                if(game.isFull()){
+                    game.printBoard();
+                    break;
+                }
+
+                game.computerTurn(game, neuralNetwork);
+
+                if(game.checkWinner() == computer) {
+                    game.printBoard();
+                    System.out.println("Not bad...but AI won!");
+                }
+            }
+        }
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,119 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class TicTacToe {
+    private static final Scanner sc = new Scanner(System.in);
+    public int[] board = new int[9];
+    private static final int human = -1;
+    private static final int computer = 1;
+    private static final int positionNotPlayed = 0;
+
+
+    public  void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            String symbol = switch (board[i]){
+                case -1 -> " X ";
+                case 1 -> " O ";
+                default -> " # ";
+            };
+            if (i == 2 || i == 5 || i == 8) {
+                System.out.println(symbol);
+            }else {
+                System.out.print(symbol);
+            }
+        }
+    }
+
+    /**
+     * @param position: Int representing the position in the board
+     * @param player: 0 to represent Human and 1 to represent the AI
+     * @return TRUE if movement was valid
+     */
+    public Boolean makeMove(int position, int player) {
+        if (board[position] == positionNotPlayed) {
+            board[position] = player;
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isFull(){
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == positionNotPlayed) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void humanTurn() {
+        printBoard();
+        System.out.print("Enter your move (0-8): ");
+        int playerMovement = sc.nextInt();
+        while(playerMovement < 0 || playerMovement > 8) {
+            System.out.print("Invalid move! Try again: ");
+            playerMovement = sc.nextInt();
+        }
+        Boolean move = makeMove(playerMovement, human);
+        while(!move) {
+            System.out.print("Invalid move! Try again: ");
+            playerMovement = sc.nextInt();
+            while (playerMovement < 0 || playerMovement > 8) {
+                System.out.print("Invalid move! Try again: ");
+                playerMovement = sc.nextInt();
+            }
+            move = makeMove(playerMovement, human);
+        }
+    }
+
+    public void computerTurn(TicTacToe game, NeuralNetwork neuralNetwork) {
+        //Input matriz
+        double[] boardInput = Arrays.stream(game.board)  // Stream<int[]>
+                .mapToDouble(x -> (double) x)                 // converte para double
+                .toArray();
+
+        double[] moveProbabilities = neuralNetwork.forward(boardInput);
+        int[] validMoves = IntStream.range(0, game.board.length)
+                        .filter(i -> game.board[i] == positionNotPlayed)
+                                .toArray();
+        int bestMove = Arrays.stream(validMoves)
+                        .boxed()
+                                .max(Comparator.comparingDouble(i -> moveProbabilities[i]))
+                                        .orElse(-1);
+
+        if (bestMove == -1) {
+            Boolean move = false;
+            while(!move) {
+                System.out.println("--- AI moved randomly ---");
+                int aiMovement = (int) (Math.random() * 9);
+                move = makeMove(aiMovement, computer);
+            }
+        }else {
+            game.makeMove(bestMove, computer);
+            System.out.println("--- AI moved based on probabilities ---");
+        }
+    }
+
+    public int checkWinner() {
+        int[][] winningCombinations = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // linhas
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // colunas
+                {0, 4, 8}, {2, 4, 6}             // diagonais
+        };
+
+        for (int[] combo : winningCombinations) {
+            int a = combo[0];
+            int b = combo[1];
+            int c = combo[2];
+
+            if (board[a] != 0 && board[a] == board[b] && board[b] == board[c]) {
+                int winner = board[a];
+                return winner; // -1 ou 1, ou seja, humano ou IA
+            }
+        }
+
+        return 0; // ninguém venceu
+    }
+}
